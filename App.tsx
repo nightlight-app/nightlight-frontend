0204306fa1c6e1e893afc2f53f467eff1cf7da4e
Zi Nean Teoh <zinean00@gmail.com>
2023-02-10 00:46:24 -0600

    run format again

--- App.tsx
+++ App.tsx
@@ -8,61 +8,61 @@ import TabBar from "./src/components/navigation/TabBar";
 const Tab = createBottomTabNavigator();
 
 const MapScreen = () => {
-	return (
-		<SafeAreaView>
-			<Text>Map</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <Text>Map</Text>
+    </SafeAreaView>
+  );
 };
 
 const SocialScreen = () => {
-	return (
-		<SafeAreaView>
-			<Text>Social</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <Text>Social</Text>
+    </SafeAreaView>
+  );
 };
 
 const PlaceholderScreen = () => {
-	return (
-		<SafeAreaView>
-			<Text>Placeholder</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <Text>Placeholder</Text>
+    </SafeAreaView>
+  );
 };
 
 const ExploreScreen = () => {
-	return (
-		<SafeAreaView>
-			<Text>Explore</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <Text>Explore</Text>
+    </SafeAreaView>
+  );
 };
 
 const ProfileScreen = () => {
-	return (
-		<SafeAreaView>
-			<Text>Profile</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+    </SafeAreaView>
+  );
 };
 
 const App = () => {
-	return (
-		<NavigationContainer>
-			<Tab.Navigator
-				initialRouteName="Map"
-				screenOptions={{ headerShown: false }}
-				tabBar={(props: any) => <TabBar {...props} />}
-			>
-				<Tab.Screen name="Map" component={MapScreen} />
-				<Tab.Screen name="Social" component={SocialScreen} />
-				<Tab.Screen name="Placeholder" component={PlaceholderScreen} />
-				<Tab.Screen name="Explore" component={ExploreScreen} />
-				<Tab.Screen name="Profile" component={ProfileScreen} />
-			</Tab.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Map"
+        screenOptions={{ headerShown: false }}
+        tabBar={(props: any) => <TabBar {...props} />}
+      >
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Social" component={SocialScreen} />
+        <Tab.Screen name="Placeholder" component={PlaceholderScreen} />
+        <Tab.Screen name="Explore" component={ExploreScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
