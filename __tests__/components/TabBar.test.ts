f3fc09175fb26f7d825a2550436d131b244201bd
Zi Nean Teoh <zinean00@gmail.com>
2023-02-10 14:26:14 -0600

    install react-test-renderer

--- __tests__/components/TabBar.test.ts
+++ __tests__/components/TabBar.test.ts
@@ -1,3 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import TabBar from 'components/navigation/TabBar';
+
 describe('<TabBar />', () => {
   test('Test', () => {
     expect(true).toBe(true);
