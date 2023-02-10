4460d41f32b37bbbdd948b6834cfb43d64202d21
Zi Nean Teoh <zinean00@gmail.com>
2023-02-10 00:41:44 -0600

    run npm format mr ethan

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-
-
+import React from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
--- src/components/navigation/NavIcon.tsx
+++ src/components/navigation/NavIcon.tsx
@@ -8,70 +8,82 @@ import { FontAwesome } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 
 enum Route {
-    MAP = "Map",
-    SOCIAL = "Social",
-    EXPLORE = "Explore",
-    PROFILE = "Profile",
+  MAP = "Map",
+  SOCIAL = "Social",
+  EXPLORE = "Explore",
+  PROFILE = "Profile",
 }
 
 interface Props {
-    route: Route;
-    isFocused: boolean;
-    size?: number;
-    color?: string;
-    focusColor?: string;
+  route: Route;
+  isFocused: boolean;
+  size?: number;
+  color?: string;
+  focusColor?: string;
 }
 
 const NavIcon = ({
-    route,
-    isFocused,
-    size = 30,
-    color = "#FFFFFF",
-    focusColor = "#4A86E8",
+  route,
+  isFocused,
+  size = 30,
+  color = "#FFFFFF",
+  focusColor = "#4A86E8",
 }: Props) => {
-    const renderIcon = (route: Route) => {
-        switch (route) {
-            case Route.MAP:
-                return <Entypo name="map" size={size} color={isFocused ? focusColor : color} />;
-            case Route.SOCIAL:
-                return (
-                    <MaterialCommunityIcons
-                        name="account-group"
-                        size={size}
-                        color={isFocused ? focusColor : color}
-                    />
-                );
-            case Route.EXPLORE:
-                return (
-                    <Ionicons name="search" size={size} color={isFocused ? focusColor : color} />
-                );
-            case Route.PROFILE:
-                return (
-                    <FontAwesome
-                        name="id-card"
-                        size={size}
-                        color={isFocused ? focusColor : color}
-                    />
-                );
-            default:
-                return <MaterialIcons name="disabled-by-default" size={size} color={color} />;
-        }
-    };
+  const renderIcon = (route: Route) => {
+    switch (route) {
+      case Route.MAP:
+        return (
+          <Entypo
+            name="map"
+            size={size}
+            color={isFocused ? focusColor : color}
+          />
+        );
+      case Route.SOCIAL:
+        return (
+          <MaterialCommunityIcons
+            name="account-group"
+            size={size}
+            color={isFocused ? focusColor : color}
+          />
+        );
+      case Route.EXPLORE:
+        return (
+          <Ionicons
+            name="search"
+            size={size}
+            color={isFocused ? focusColor : color}
+          />
+        );
+      case Route.PROFILE:
+        return (
+          <FontAwesome
+            name="id-card"
+            size={size}
+            color={isFocused ? focusColor : color}
+          />
+        );
+      default:
+        return (
+          <MaterialIcons name="disabled-by-default" size={size} color={color} />
+        );
+    }
+  };
 
-    return (
-        <View
-            style={
-                isFocused && {
-                    shadowColor: focusColor,
-                    shadowOffset: { width: 0, height: 0 },
-                    shadowOpacity: 0.5,
-                    shadowRadius: 4,
-                }
-            }
-        >
-            {renderIcon(route)}
-        </View>
-    );
+  return (
+    <View
+      style={
+        isFocused && {
+          shadowColor: focusColor,
+          shadowOffset: { width: 0, height: 0 },
+          shadowOpacity: 0.5,
+          shadowRadius: 4,
+        }
+      }
+    >
+      {renderIcon(route)}
+    </View>
+  );
 };
 
 export default NavIcon;
