294cc456ced960abeeb8f1878e74937bd2c64b91
Zi Nean Teoh <zinean00@gmail.com>
2023-02-16 21:51:35 -0600

    implement mapview and camera

--- src/components/map/NightlightMap.tsx
+++ src/components/map/NightlightMap.tsx
@@ -1,16 +1,56 @@
 import { StyleSheet, View, Dimensions } from 'react-native';
-import MapboxGL from '@rnmapbox/maps';
+import MapboxGL, {
+  Camera,
+  CameraStop,
+  MapView,
+  MapViewProps,
+} from '@rnmapbox/maps';
 import { MAPBOX_API_KEY } from '@env';
+import { useEffect, useState } from 'react';
 
 const { width, height } = Dimensions.get('window');
 
+// initial camera settings
+const initialCamera: CameraStop = {
+  // centered at Vanderbilt University
+  centerCoordinate: [-86.78472854186322, 36.157845061262364],
+  animationDuration: 0,
+  zoomLevel: 16,
+  pitch: 50,
+  heading: 62,
+};
+
+// pass the api key to Mapbox
 MapboxGL.setAccessToken(MAPBOX_API_KEY);
 
 const NightlightMap = () => {
+  // reference to MapboxGL's map view
+  const [mapView, setMapView] = useState<MapView>();
+
+  // reference to MapboxGL's camera
+  const [camera, setCamera] = useState<Camera>();
+
+  // set the initial camera
+  useEffect(() => {
+    if (camera) {
+      camera.setCamera(initialCamera);
+    }
+  }, [camera]);
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <MapboxGL.MapView style={styles.map} scaleBarEnabled={false} />
+        <MapboxGL.MapView
+          ref={m => {
+            if (!mapView && m) setMapView(mapView);
+          }}
+          {...mapviewProps}>
+          {/* TODO: Add markers over */}
+          <Camera
+            ref={c => {
+              if (!camera && c) setCamera(c);
+            }}></Camera>
+        </MapboxGL.MapView>
       </View>
     </View>
   );
@@ -31,5 +71,16 @@ const styles = StyleSheet.create({
     height: height,
   },
 });
+
+// the map view props to pass into
+const mapviewProps: MapViewProps = {
+  scaleBarEnabled: false,
+  style: styles.map,
+  styleURL: MapboxGL.StyleURL.Dark,
+  scrollEnabled: true,
+  pitchEnabled: true,
+  rotateEnabled: true,
+  compassEnabled: true,
+};
 
 export default NightlightMap;
