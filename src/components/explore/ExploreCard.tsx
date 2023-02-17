c50b01d2a2d0f63c2c9a761e3ab1afcde48cda9c
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-02-17 16:32:28 -0600

    Run formatter

--- src/components/explore/ExploreCard.tsx
+++ src/components/explore/ExploreCard.tsx
@@ -14,32 +14,32 @@ type Props = {
 };
 
 const ExploreCard = (Props: Props) => {
-    let [fontsLoaded] = useFonts({
-        Comfortaa_600SemiBold,
-        Comfortaa_700Bold,
-      });
-    
-      if (fontsLoaded) {
-        return (
-          <View style={styles.container}>
-            <View style={styles.titleContainer}>
-              <Text style={styles.venueTitle}>{Props.name}</Text>
+  let [fontsLoaded] = useFonts({
+    Comfortaa_600SemiBold,
+    Comfortaa_700Bold,
+  });
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.venueTitle}>{Props.name}</Text>
+        </View>
+        <View style={styles.lowerContainer}>
+          <View style={styles.reactionContainer}>
+            <Text style={styles.venueAddress}>{Props.address}</Text>
+            <View style={styles.reactionGroup}>
+              <VenueReaction emoji='🔥' value={8} active={false} />
+              <VenueReaction emoji='😄' value={2} active={true} />
+              <VenueReaction emoji='🎉' value={12} active={false} />
+              <VenueReaction emoji='⚠️' value={5} active={false} />
+              <VenueReaction emoji='💩' value={11} active={false} />
             </View>
-            <View style={styles.lowerContainer}>
-              <View style={styles.reactionContainer}>
-                <Text style={styles.venueAddress}>{Props.address}</Text>
-                <View style={styles.reactionGroup}>
-                  <VenueReaction emoji='🔥' value={8} active={false} />
-                  <VenueReaction emoji='😄' value={2} active={true} />
-                  <VenueReaction emoji='🎉' value={12} active={false} />
-                  <VenueReaction emoji='⚠️' value={5} active={false} />
-                  <VenueReaction emoji='💩' value={11} active={false} />
-                </View>
-              </View>
-              <Pressable style={styles.goButton}>
-                <Text style={styles.goButtonText}>GO</Text>
-                <Text style={styles.goButtonSubText}>{Props.lat}</Text>
-              </Pressable>
+          </View>
+          <Pressable style={styles.goButton}>
+            <Text style={styles.goButtonText}>GO</Text>
+            <Text style={styles.goButtonSubText}>{Props.lat}</Text>
+          </Pressable>
         </View>
       </View>
     );
