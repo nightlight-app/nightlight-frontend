861d9a6f056ffa46407dbbdbcfc6f93b34ad1e3e
Zi Nean Teoh <zinean00@gmail.com>
2023-02-20 00:12:11 -0500

    implement compass and follow user button

--- src/components/map/NightlightMap.tsx
+++ src/components/map/NightlightMap.tsx
@@ -1,18 +1,23 @@
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, Dimensions, Pressable } from 'react-native';
 import MapboxGL, {
   Camera,
   CameraStop,
+  MapboxGLEvent,
   MapView,
   MapViewProps,
 } from '@rnmapbox/maps';
 import { MAPBOX_API_KEY } from '@env';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import React from 'react';
+import MapScreenStyles from '@nightlight/screens/map/MapScreen.styles';
+import { COLORS } from '@nightlight/src/global.styles';
+import { Ionicons } from '@expo/vector-icons';
 
 const { width, height } = Dimensions.get('window');
 
 // initial camera settings
 const initialCamera: CameraStop = {
-  // centered at Vanderbilt University
+  // centered at Vanderbilt University [longitude, latitude]
   centerCoordinate: [-86.78472854186322, 36.157845061262364],
   animationDuration: 0,
   zoomLevel: 16,
@@ -28,14 +33,47 @@ const NightlightMap = () => {
   const [mapView, setMapView] = useState<MapView>();
 
   // reference to MapboxGL's camera
-  const [camera, setCamera] = useState<Camera>();
+  const camera = useRef<Camera>(null);
 
-  // set the initial camera
+  // keep track of the user's current location
+  const [userLocation, setUserLocation] = useState<MapboxGL.Location>();
+
+  // whether the camera is following user (both location and device heading)
+  const [isCameraFollowingUser, setIsCameraFollowingUser] =
+    useState<boolean>(false);
+
+  // set the initial camera on first load
   useEffect(() => {
-    if (camera) {
-      camera.setCamera(initialCamera);
+    camera.current?.setCamera(initialCamera);
+  }, [camera.current]);
+
+  // reset camera to user position
+  const toggleCameraFollow = () => setIsCameraFollowingUser(prev => !prev);
+
+  // reset camera heading to north
+  const resetCameraHeadingToNorth = () => {
+    if (camera.current) {
+      camera.current.setCamera({
+        heading: 0,
+        animationDuration: 1000,
+        animationMode: 'easeTo',
+      });
     }
-  }, [camera]);
+  };
+
+  /**
+   * Handle the onUserTrackingModeChange event emitted by Mapbox Camera.
+   */
+  const handleUserTrackingModeChange = (event: {
+    nativeEvent: { payload: { followUserMode: string | null } };
+  }) => {
+    // camera is currently following user
+    if (isCameraFollowingUser) {
+      if (event.nativeEvent.payload.followUserMode === null)
+        // stop camera from following user
+        setIsCameraFollowingUser(false);
+    }
+  };
 
   return (
     <View style={styles.page}>
@@ -47,9 +85,10 @@ const NightlightMap = () => {
           {...mapviewProps}>
           {/* Camera */}
           <Camera
-            ref={c => {
-              if (!camera && c) setCamera(c);
-            }}
+            ref={camera}
+            onUserTrackingModeChange={handleUserTrackingModeChange}
+            followUserLocation={isCameraFollowingUser}
+            followHeading={userLocation?.coords.heading}
           />
 
           {/* UserMarker */}
@@ -57,13 +96,31 @@ const NightlightMap = () => {
             showsUserHeadingIndicator={true}
             renderMode={'native'}
             visible={true}
-            onUpdate={p => {
-              console.log('here: ', p);
-            }}>
-            <MapboxGL.CircleLayer id='myUser' />
+            minDisplacement={5}
+            onUpdate={loc => setUserLocation(loc)}>
+            {/* TODO: add and style the user circle marker */}
+            <MapboxGL.CircleLayer id='userMarker' />
           </MapboxGL.UserLocation>
         </MapboxGL.MapView>
       </View>
+
+      {/* Interactable Buttons */}
+      <Pressable
+        onPress={resetCameraHeadingToNorth}
+        style={[
+          MapScreenStyles.mapControlButton,
+          MapScreenStyles.orientMapButton,
+        ]}>
+        <Ionicons name='md-compass' size={32} color={COLORS.WHITE} />
+      </Pressable>
+      <Pressable
+        onPress={toggleCameraFollow}
+        style={[
+          MapScreenStyles.mapControlButton,
+          MapScreenStyles.currentLocationButton,
+        ]}>
+        <Ionicons name='md-navigate' size={32} color={COLORS.NIGHTLIGHT_BLUE} />
+      </Pressable>
     </View>
   );
 };
@@ -73,7 +130,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 15,
   },
   container: {
     height: height,
