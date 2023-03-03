5e8b6172432894289f3aa84e1bee17b33375df9c
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-03-02 21:22:24 -0600

    Major profile refactor

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,140 +1,182 @@
 import React from 'react';
 import {
-  ImageBackground,
   Text,
   View,
   Image,
   ScrollView,
-  Pressable,
+  TouchableOpacity,
+  SafeAreaView,
 } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import EmergencyContactsScreen from '@nightlight/screens/profile/EmergencyContactsScreen';
 import ProfileScreenStyles from '@nightlight/screens/profile/ProfileScreen.styles';
 import PartySvg from '@nightlight/components/svgs/PartySvg';
 import BottleSvg from '@nightlight/components/svgs/BottleSvg';
-import PencilSvg from '@nightlight/components/svgs/PencilSvg';
-import SettingsSvg from '@nightlight/components/svgs/SettingsSvg';
-import PhotoSvg from '@nightlight/components/svgs/PhotoSvg';
-import EmergencyContactsScreen from '@nightlight/screens/profile/EmergencyContactsScreen';
 import { ProfileRoute, ProfileScreenProps } from '@nightlight/src/types';
 
-const ProfileScreen = ({ navigation }: ProfileScreenProps) => {
-  // TODO: query profile rather than hardcoding
-  let ProfileInfo = {
-    name: 'John Smith',
-    phone: '(123) 456-7891',
-    friends: 12,
-    nightsOut: 6,
-    birthday: '01.01.2001',
-    favoriteBar: 'Underground',
-  };
+// TODO: should this be in utils?
+/**
+ * Determines the text to display for a given month index
+ *
+ * @param index The index of the month (0-11)
+ * @returns The string 'Jan' for index 0, 'Dec' for index 11, and the index + 1 for all other indices
+ */
+const getMonthText = (index: number) => {
+  switch (index) {
+    case 0:
+      return 'Jan';
+    case 11:
+      return 'Dec';
+    default:
+      return index + 1;
+  }
+};
 
-  const getMonthText = (index: number) => {
-    switch (index) {
-      case 0:
-        return 'Jan';
-      case 11:
-        return 'Dec';
-      default:
-        return index + 1;
-    }
+// TODO: determine logged in user
+const user = TEST_USERS[0];
+
+const ProfileScreen = ({ navigation }: ProfileScreenProps) => {
+  // TODO:
+  const handleChangeCoverPicture = () => {};
+
+  // TODO:
+  const handleSettingsPress = () => {};
+
+  // TODO:
+  const handleEditProfile = () => {};
+
+  // TODO:
+  const handleNavigateToEmergencyContacts = () => {
+    navigation.navigate(ProfileRoute.EMERGENCY_CONTACTS);
   };
 
   return (
-    <View testID={ProfileRoute.PROFILE} style={ProfileScreenStyles.container}>
-      {/* Background image */}
-      <View style={ProfileScreenStyles.backgroundImageContainer}>
-        <ImageBackground
-          source={require('@nightlight/assets/images/cover-photo.png')}
-          style={ProfileScreenStyles.backgroundImage}
-        />
-        {/* Interactable SVGs (TODO: make these interactable) */}
-        <SettingsSvg style={ProfileScreenStyles.settingsSVG} />
-        <PhotoSvg style={ProfileScreenStyles.photoSvg} />
-        <PencilSvg style={ProfileScreenStyles.pencilSVG} />
-      </View>
-
-      {/* Profile info */}
-      <View style={ProfileScreenStyles.profileInfoContainer}>
+    <ScrollView
+      testID={ProfileRoute.PROFILE}
+      contentContainerStyle={ProfileScreenStyles.scrollViewContainer}>
+      {/* Cover Picture */}
+      <View style={ProfileScreenStyles.coverPicContainer}>
         <Image
-          source={require('@nightlight/assets/images/anon.png')}
-          style={ProfileScreenStyles.profileImage}
+          style={ProfileScreenStyles.coverPic}
+          // TODO: make this dynamic
+          source={require('@nightlight/assets/images/cover-photo.png')}
         />
-        <Text style={ProfileScreenStyles.name}>{ProfileInfo.name}</Text>
-        <Text style={ProfileScreenStyles.number}>{ProfileInfo.phone}</Text>
+        {/* Change Cover Picture */}
+        <TouchableOpacity
+          onPress={handleChangeCoverPicture}
+          activeOpacity={0.5}
+          style={ProfileScreenStyles.changeCoverButton}>
+          <FontAwesome name='picture-o' size={25} color={COLORS.WHITE} />
+        </TouchableOpacity>
       </View>
 
-      {/* Scrollable view */}
-      <ScrollView
-        showsVerticalScrollIndicator={true}
-        style={ProfileScreenStyles.scrollView}>
-        {/* Profile statistics */}
-        <View style={ProfileScreenStyles.profileStatisticsContainer}>
-          <View style={ProfileScreenStyles.box}>
-            <Text style={ProfileScreenStyles.numberText}>
-              {ProfileInfo.friends}
+      {/* Settings Button */}
+      <SafeAreaView style={ProfileScreenStyles.settingsButtonContainer}>
+        <TouchableOpacity
+          onPress={handleSettingsPress}
+          activeOpacity={0.5}
+          style={ProfileScreenStyles.settingsButton}>
+          <Ionicons name='settings' size={25} color={COLORS.WHITE} />
+        </TouchableOpacity>
+      </SafeAreaView>
+
+      {/* Profile Picture */}
+      {/* TODO: add press action?? (expand or edit?) */}
+      <Image
+        source={{ uri: user.imgUrlProfileSmall }}
+        style={ProfileScreenStyles.profilePic}
+      />
+
+      {/* Profile Details */}
+      <View style={ProfileScreenStyles.profileDetailsContainer}>
+        {/* Edit Profile Button */}
+        <TouchableOpacity
+          onPress={handleEditProfile}
+          activeOpacity={0.5}
+          style={ProfileScreenStyles.editProfileButton}>
+          <FontAwesome name='pencil' size={25} color={COLORS.WHITE} />
+        </TouchableOpacity>
+
+        {/* User Info */}
+        <Text style={ProfileScreenStyles.name}>
+          {user.firstName} {user.lastName}
+        </Text>
+        <Text style={ProfileScreenStyles.phoneNumber}>
+          {formatPhoneNumber(user.phone)}
+        </Text>
+
+        {/* Profile Statistics */}
+        <View style={ProfileScreenStyles.profileStatsContainer}>
+          <View style={ProfileScreenStyles.profileStatContainer}>
+            <Text style={ProfileScreenStyles.profileStat}>
+              {user.friends.length}
             </Text>
-            <Text style={ProfileScreenStyles.smallText}>friends</Text>
+            <Text style={ProfileScreenStyles.profileStatDesc}>friends</Text>
           </View>
-          <View style={ProfileScreenStyles.box}>
-            <Text style={ProfileScreenStyles.numberText}>
-              {ProfileInfo.nightsOut}
+          <View style={ProfileScreenStyles.profileStatDivider} />
+          <View style={ProfileScreenStyles.profileStatContainer}>
+            <Text style={ProfileScreenStyles.profileStat}>
+              {/* TODO: need field on user object */}
+              {2}
             </Text>
-            <Text style={ProfileScreenStyles.smallText}>nights out</Text>
+            <Text style={ProfileScreenStyles.profileStatDesc}>nights out</Text>
           </View>
-          <View style={ProfileScreenStyles.box}>
-            <Text style={ProfileScreenStyles.numberText}>
-              {ProfileInfo.birthday}
+          <View style={ProfileScreenStyles.profileStatDivider} />
+          <View style={ProfileScreenStyles.profileStatContainer}>
+            <Text style={ProfileScreenStyles.profileStat}>
+              {user.birthday.toLocaleDateString('en-US', { timeZone: 'UTC' })}
             </Text>
-            <Text style={ProfileScreenStyles.smallText}>your special day</Text>
+            <Text style={ProfileScreenStyles.profileStatDesc}>
+              your special day
+            </Text>
           </View>
         </View>
 
-        {/* Favorite bar */}
-        <View style={ProfileScreenStyles.favoriteBar}>
-          <PartySvg style={ProfileScreenStyles.partySvg} />
-          <View style={ProfileScreenStyles.barInfo}>
-            <Text style={ProfileScreenStyles.barText}>
-              {ProfileInfo.favoriteBar}
-            </Text>
-            <Text style={ProfileScreenStyles.smallText}>
-              is your favorite bar these days
+        {/* Favorite Bar */}
+        <View style={ProfileScreenStyles.favoriteBarContainer}>
+          <PartySvg />
+          <View style={ProfileScreenStyles.favoriteBarTextContainer}>
+            {/* TODO: need field on user doc */}
+            <Text style={ProfileScreenStyles.favoriteBarText}>Underground</Text>
+            <Text style={ProfileScreenStyles.favoriteBarDesc}>
+              seems to be your favorite bar these days
             </Text>
           </View>
         </View>
 
         {/* Calendar */}
-        <View style={ProfileScreenStyles.calendarView}>
-          <View style={ProfileScreenStyles.calendarContainer}>
-            {[...Array(NUM_MONTHS)].map((_, index) => (
-              <View key={index} style={ProfileScreenStyles.monthView}>
-                <BottleSvg />
-                <Text style={ProfileScreenStyles.monthText}>
-                  {getMonthText(index)}
-                </Text>
-              </View>
-            ))}
-          </View>
+        <View style={ProfileScreenStyles.calendarContainer}>
+          {[...Array(NUM_MONTHS)].map((_, index) => (
+            <View key={index} style={ProfileScreenStyles.monthView}>
+              <BottleSvg />
+              <Text style={ProfileScreenStyles.monthText}>
+                {getMonthText(index)}
+              </Text>
+            </View>
+          ))}
         </View>
 
-        {/* Emergency Contacts button */}
-        <View style={ProfileScreenStyles.emergencyView}>
-          <Pressable
-            style={ProfileScreenStyles.emergencyPressable}
-            onPress={() =>
-              navigation.navigate(ProfileRoute.EMERGENCY_CONTACTS)
-            }>
-            <Text style={ProfileScreenStyles.emergencyText}>
-              See Emergency Contacts
-            </Text>
-          </Pressable>
-        </View>
-      </ScrollView>
-    </View>
+        {/* Emergency Contacts Button */}
+        <TouchableOpacity
+          onPress={handleNavigateToEmergencyContacts}
+          activeOpacity={0.75}
+          style={ProfileScreenStyles.emergencyContactsButton}>
+          <Text style={ProfileScreenStyles.emergencyContactsButtonText}>
+            See Emergency Contacts
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
 // Create new stack navigator
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NUM_MONTHS } from '@nightlight/src/constants';
+import { COLORS } from '@nightlight/src/global.styles';
+import { TEST_USERS } from '@nightlight/src/testData';
+import { formatPhoneNumber } from '@nightlight/src/utils/utils';
 
 const Stack = createNativeStackNavigator();
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -68,3 +68,24 @@ export const capitalizeFirstLetter = (word: string) => {
 export const convertCoordinateToPosition = (
   coor: MapboxGL.Coordinates
 ): Position => [coor.longitude, coor.latitude];
+
+/**
+ * Formats a phone number string. Returns null if input is less than 4 characters or if
+ *
+ * @param {string} phoneNumberString The phone number string to format
+ * @returns A formatted phone number string
+ */
+export const formatPhoneNumber = (phoneNumberString: string) => {
+  const cleaned = phoneNumberString.replace(/\D/g, '');
+
+  if (cleaned.length < 4) return null;
+
+  const match = cleaned.match(/(\d{3})(\d{1,3})?(\d{1,4})?$/);
+
+  if (match)
+    return `(${match[1]})${match[2] !== undefined ? ` ${match[2]}` : ''}${
+      match[3] !== undefined ? `-${match[3]}` : ''
+    }`;
+
+  return null;
+};
