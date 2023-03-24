2fa0eadd206071cf6ac37291a326a6ef56000f36
Zi Nean Teoh <zinean00@gmail.com>
2023-03-24 16:13:40 -0500

    use constants to fix e2e test

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,3 +1,4 @@
+import { TEST_IDS } from '@nightlight/src/constants';
 import { by, device, expect, element, waitFor } from 'detox';
 
 describe('Example', () => {
@@ -11,11 +12,11 @@ describe('Example', () => {
 
   it('should have sign in screen', async () => {
     await expect(element(by.text('Sign in with email'))).toBeVisible();
-    await expect(element(by.id('signin-email-input'))).toExist();
-    await expect(element(by.id('signin-password-input'))).toExist();
-    await expect(element(by.id('signin-password-visibility'))).toExist();
-    await expect(element(by.id('signin-forgot-password-button'))).toExist();
-    await expect(element(by.id('signin-button'))).toExist();
-    await expect(element(by.id('signin-signup-button'))).toExist();
+    await expect(element(by.id(TEST_IDS.SIGNIN_EMAIL_INPUT))).toExist();
+    await expect(element(by.id(TEST_IDS.SIGNIN_PASSWORD_INPUT))).toExist();
+    await expect(element(by.id(TEST_IDS.SIGNIN_PASSWORD_VISIBILITY))).toExist();
+    await expect(element(by.id(TEST_IDS.SIGNIN_FORGOT_PASSWORD))).toExist();
+    await expect(element(by.id(TEST_IDS.SIGNIN_BUTTON))).toExist();
+    await expect(element(by.id(TEST_IDS.SIGNUP_BUTTON))).toExist();
   });
 });
