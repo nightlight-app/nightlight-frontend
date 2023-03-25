01a2236375b65e18ad617ad7972e34da9e6284e3
jacoblurie29 <jacoblurie29@gmail.com>
2023-03-24 21:01:42 -0500

    Removed unnecessary notification function

--- src/App.tsx
+++ src/App.tsx
@@ -62,31 +62,6 @@ setNotificationHandler({
   }),
 });
 
-const sendPushNotification = async (
-  expoPushToken: string,
-  title: string,
-  body: string,
-  data: any
-) => {
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: title,
-    body: body,
-    data: data,
-  };
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  });
-};
-
 const registerForPushNotificationsAsync = async () => {
   // Retrieve the project ID from the Expo config
   const projectId = Constants.expoConfig?.extra?.eas.projectId;
