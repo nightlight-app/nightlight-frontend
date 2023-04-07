bd532af9d24c4f6bddb6e21f312f14602a03754f
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-07 01:55:06 -0500

    Refactor location/heading following

--- src/components/map/NightlightMap.tsx
+++ src/components/map/NightlightMap.tsx
@@ -2,6 +2,7 @@ import { MAPBOX_API_KEY, SERVER_URL } from '@env';
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Pressable, Image } from 'react-native';
 import MapboxGL, { Camera, CameraStop } from '@rnmapbox/maps';
+import { UserTrackingMode } from '@rnmapbox/maps/javascript/components/Camera';
 import { Position } from '@turf/helpers/dist/js/lib/geojson';
 import { MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons';
 import MapScreenStyles from '@nightlight/screens/map/MapScreen.styles';
@@ -17,7 +18,8 @@ import { useAuthContext } from '@nightlight/src/contexts/AuthContext';
 
 // initial camera settings
 const initialCamera: CameraStop = {
-  animationDuration: 0,
+  animationDuration: 500,
+  animationMode: 'flyTo',
   zoomLevel: 16,
   pitch: 0,
   heading: 0,
@@ -28,24 +30,18 @@ MapboxGL.setAccessToken(MAPBOX_API_KEY);
 
 const NightlightMap = ({ onError }: NightlightMapProps) => {
   const { userDocument } = useAuthContext();
+  const groupId = userDocument?.currentGroup;
 
   /**
    * Location Tracking Variables
    */
-  const groupId = userDocument?.currentGroup;
-
-  // the map of user id to UserMarker (excluding the current user)
-  const [userMarkers, setUserMarkers] = useState<UserMarkerMap>({});
-
-  // reference to MapboxGL's camera
-  const camera = useRef<Camera>(null);
-
-  // keep track of the user's current location
-  const [userLocation, setUserLocation] = useState<MapboxGL.Location>();
-
-  // whether the camera is following user (both location and device heading)
-  const [isCameraFollowingUser, setIsCameraFollowingUser] =
-    useState<boolean>(true);
+  const [userMarkers, setUserMarkers] = useState<UserMarkerMap>({}); // map of user id to UserMarker (excluding the current user)
+  const camera = useRef<Camera>(null); // reference to MapboxGL's camera
+  const [userLocation, setUserLocation] = useState<MapboxGL.Location>(); // user's current location
+  const [isCameraFollowingUserLocation, setIsCameraFollowingUserLocation] =
+    useState<boolean>(true); // whether the camera is following user's location
+  const [isCameraFollowingUserHeading, setIsCameraFollowingUserHeading] =
+    useState<boolean>(false); // whether the camera is following user's heading
 
   // set up socket on first mount
   useEffect(() => {
@@ -53,7 +49,7 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
     if (!groupId) return;
 
     console.log(
-      `[Map] Setting up socket for user ${userDocument?.firstName} ${userDocument?.lastName}}`
+      `[Map] Setting up socket for user ${userDocument?.firstName} ${userDocument?.lastName}...`
     );
 
     // tell server to add this user to a socket group
@@ -101,14 +97,13 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
     return () => {
       // leave the group so the server stops sending location updates to this user
       socket.emit('leaveGroup', groupId);
-      // disconnect from the socket
-      socket.off('');
+      socket.off(''); // disconnect from the socket
     };
   }, [groupId, userDocument]);
 
   // set the initial camera to user's location on first load
   useEffect(() => {
-    if (userLocation && isCameraFollowingUser) {
+    if (userLocation && isCameraFollowingUserLocation) {
       const { longitude, latitude } = userLocation.coords;
       const position: Position = [longitude, latitude];
 
@@ -117,32 +112,30 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
         centerCoordinate: position,
       });
     }
-  }, [camera.current]);
+  }, []);
 
-  // reset camera heading to north when camera is no longer following user
-  useEffect(() => {
-    if (!isCameraFollowingUser) {
-      resetCameraHeadingToNorth();
-    }
-  }, [isCameraFollowingUser]);
+  // FIXME: this is not working
+  // useEffect(() => {
+  //   // if camera changes to not follow user heading, face north
+  //   if (!isCameraFollowingUserHeading) {
+  //     console.log('should face north');
+  //     camera.current?.setCamera({
+  //       ...initialCamera,
+  //       // heading: 0,
+  //       // animationDuration: 500,
+  //       // animationMode: 'easeTo',
+  //     });
+  //   }
+  // }, [isCameraFollowingUserHeading]);
 
-  /**
-   * Toggle the isCameraFollowingUser variable
-   */
-  const toggleCameraFollow = () => {
-    setIsCameraFollowingUser(prev => !prev);
-  };
-
-  /**
-   * Reset the camera's heading to point to North
-   */
-  const resetCameraHeadingToNorth = () => {
-    if (camera.current) {
-      camera.current.setCamera({
-        heading: 0,
-        animationDuration: 500,
-        animationMode: 'easeTo',
-      });
+  //
+  const handleLocationButtonPress = () => {
+    if (isCameraFollowingUserLocation) {
+      // if camera is already following user, toggle following user heading
+      setIsCameraFollowingUserHeading(prev => !prev);
+    } else {
+      // if camera is not following user, start following user
+      setIsCameraFollowingUserLocation(true);
     }
   };
 
@@ -158,11 +151,11 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
   const handleUserTrackingModeChange = (event: {
     nativeEvent: { payload: { followUserMode: string | null } };
   }) => {
-    // camera is currently following user
-    if (isCameraFollowingUser) {
-      if (event.nativeEvent.payload.followUserMode === null)
-        // stop camera from following user
-        setIsCameraFollowingUser(false);
+    const { followUserMode } = event.nativeEvent.payload;
+
+    if (isCameraFollowingUserLocation && followUserMode === null) {
+      setIsCameraFollowingUserLocation(false);
+      setIsCameraFollowingUserHeading(false);
     }
   };
 
@@ -189,10 +182,10 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
     <View style={NightlightMapStyles.page}>
       <View style={NightlightMapStyles.container}>
         <MapboxGL.MapView
-          zoomEnabled={true}
-          scaleBarEnabled={false}
           style={NightlightMapStyles.map}
           styleURL={MapboxGL.StyleURL.Dark}
+          zoomEnabled={true}
+          scaleBarEnabled={false}
           scrollEnabled={true}
           pitchEnabled={false}
           rotateEnabled={false}
@@ -201,8 +194,15 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
           <Camera
             ref={camera}
             onUserTrackingModeChange={handleUserTrackingModeChange}
-            followUserLocation={isCameraFollowingUser}
-            followHeading={userLocation?.coords.heading}
+            followUserLocation={isCameraFollowingUserLocation}
+            followHeading={isCameraFollowingUserHeading ? undefined : 0} // FIXME: this is solution to facing north when not following user heading
+            followUserMode={
+              isCameraFollowingUserLocation
+                ? isCameraFollowingUserHeading
+                  ? UserTrackingMode.FollowWithHeading
+                  : UserTrackingMode.Follow
+                : undefined
+            }
           />
 
           {/* UserLocation tracker */}
@@ -260,14 +260,21 @@ const NightlightMap = ({ onError }: NightlightMapProps) => {
 
         {/* User Attach Button */}
         <Pressable
-          onPress={toggleCameraFollow}
+          onPress={handleLocationButtonPress}
           style={MapScreenStyles.mapControlButton}>
           <MaterialCommunityIcons
             name={`navigation-variant${
-              isCameraFollowingUser ? '' : '-outline'
+              isCameraFollowingUserLocation ? '' : '-outline'
             }`}
             size={32}
             color={COLORS.NIGHTLIGHT_BLUE}
+            style={{
+              transform: [
+                {
+                  rotate: `${isCameraFollowingUserHeading ? -45 : 0}deg`,
+                },
+              ],
+            }}
           />
         </Pressable>
       </View>
