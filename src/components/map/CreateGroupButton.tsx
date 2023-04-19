b6b5b3edff2c7ebf771aa16fef35840ffaaaf5f4
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-18 19:14:02 -0500

    Add button animation

--- src/components/map/CreateGroupButton.tsx
+++ src/components/map/CreateGroupButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableHighlight, SafeAreaView, Text } from 'react-native';
+import Animated, { SlideInLeft, SlideOutLeft } from 'react-native-reanimated';
 import { ButtonProps } from '@nightlight/src/types';
 import CreateGroupButtonStyles from '@nightlight/components/map/CreateGroupButton.styles';
 import { COLORS } from '@nightlight/src/global.styles';
@@ -7,13 +8,15 @@ import { COLORS } from '@nightlight/src/global.styles';
 const CreateGroupButton = ({ onPress }: ButtonProps) => {
   return (
     <SafeAreaView style={CreateGroupButtonStyles.container}>
-      <TouchableHighlight
-        onPress={onPress}
-        style={CreateGroupButtonStyles.button}
-        underlayColor={COLORS.NIGHTLIGHT_GRAY}
-        activeOpacity={0.5}>
-        <Text style={CreateGroupButtonStyles.buttonText}>+ Create Group</Text>
-      </TouchableHighlight>
+      <Animated.View entering={SlideInLeft} exiting={SlideOutLeft}>
+        <TouchableHighlight
+          onPress={onPress}
+          style={CreateGroupButtonStyles.button}
+          underlayColor={COLORS.NIGHTLIGHT_GRAY}
+          activeOpacity={0.5}>
+          <Text style={CreateGroupButtonStyles.buttonText}>+ Create Group</Text>
+        </TouchableHighlight>
+      </Animated.View>
     </SafeAreaView>
   );
 };
