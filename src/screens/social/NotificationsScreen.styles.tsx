7c9d325f2cbb6c0a66f5a4f5af0881e42dc7e2c3
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-22 18:06:03 -0500

    Remove comments

--- src/screens/social/NotificationsScreen.styles.tsx
+++ src/screens/social/NotificationsScreen.styles.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 import {
   DEVICE_HEIGHT,
-  DEVICE_WIDTH,
   EMERGENCY_BUTTON_RADIUS,
   NAVBAR_HEIGHT,
 } from '@nightlight/src/constants';
@@ -62,68 +61,4 @@ export default StyleSheet.create({
   notificationCardSeparator: {
     height: 2,
   },
-
-  // screenContainer: {
-  //   backgroundColor: COLORS.NIGHTLIGHT_GRAY,
-  //   minHeight: '100%',
-  // },
-  // title: {
-  //   fontFamily: Fonts.COMFORTAA_BOLD,
-  //   fontSize: 24,
-  //   color: COLORS.WHITE,
-  //   textAlign: 'center',
-  //   marginVertical: 10,
-  // },
-  // contactSeparator: {
-  //   height: 2,
-  // },
-  // emptyContactsContainer: {
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
-  // emptyAvailableUsersText: {
-  //   color: COLORS.GRAY,
-  //   fontFamily: Fonts.COMFORTAA_BOLD,
-  //   fontSize: 14,
-  //   textAlign: 'center',
-  //   marginTop: 20,
-  //   marginBottom: 15,
-  //   marginHorizontal: 50,
-  //   lineHeight: 18,
-  // },
-  // topRow: {
-  //   display: 'flex',
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   justifyContent: 'flex-start',
-  //   paddingLeft: '5%',
-  // },
-  // notifCircle: {
-  //   width: 25,
-  //   height: 25,
-  //   borderRadius: DEVICE_WIDTH / 2,
-  //   backgroundColor: COLORS.RED,
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  //   marginLeft: '3%',
-  // },
-  // numberText: {
-  //   fontFamily: Fonts.COMFORTAA_BOLD,
-  //   fontSize: 14,
-  //   color: COLORS.WHITE,
-  // },
-  // notifList: {
-  //   // justifyContent: 'center',
-  //   // alignItems: 'center',
-  //   width: '100%',
-  //   paddingBottom: NAVBAR_HEIGHT + EMERGENCY_BUTTON_RADIUS + 10,
-  // },
-  // notifListContent: {
-  //   paddingBottom: NAVBAR_HEIGHT + EMERGENCY_BUTTON_RADIUS + 20,
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // notifCardSeparator: {
-  //   height: 2,
-  // },
 });
