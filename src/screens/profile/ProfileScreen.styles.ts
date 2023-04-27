84a969df7ea9880e7df9256c81ffad34a82a3fea
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-27 17:54:24 -0500

    Reorganize profile info

--- src/screens/profile/ProfileScreen.styles.ts
+++ src/screens/profile/ProfileScreen.styles.ts
@@ -11,29 +11,22 @@ export default StyleSheet.create({
   scrollView: {
     backgroundColor: COLORS.NIGHTLIGHT_GRAY,
   },
-  scrollViewContainer: {
-    minHeight: '100%',
-  },
   coverPicContainer: {
     width: '100%',
+    // borderBottomWidth: 2,
+    // borderBottomColor: COLORS.DARK_GRAY,
   },
   coverPic: {
     width: '100%',
     height: DEVICE_HEIGHT * 0.2,
   },
-  changeCoverButton: {
-    position: 'absolute',
-    bottom: 0,
-    padding: 10,
-    opacity: 0.75,
+  contentContainer: {
+    paddingHorizontal: 25,
   },
-  settingsButtonContainer: {
-    position: 'absolute',
-    right: 0,
-    opacity: 0.75,
-  },
-  settingsButton: {
-    padding: 10,
+  contentHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
   },
   profilePic: {
     height: 150,
@@ -41,9 +34,7 @@ export default StyleSheet.create({
     borderRadius: 150 / 2,
     borderWidth: 3,
     borderColor: COLORS.WHITE,
-    alignSelf: 'center',
-    position: 'absolute',
-    top: DEVICE_HEIGHT * 0.2 - 150 / 2,
+    marginTop: -150 / 2,
     backgroundColor: COLORS.NIGHTLIGHT_BLACK,
     justifyContent: 'center',
     alignItems: 'center',
@@ -53,76 +44,127 @@ export default StyleSheet.create({
     fontFamily: Fonts.COMFORTAA_BOLD,
     color: COLORS.WHITE,
   },
-  profileDetailsContainer: {
-    paddingTop: 150 / 2 + 10,
-    alignItems: 'center',
-  },
   editProfileButton: {
-    position: 'absolute',
-    alignSelf: 'flex-end',
-    padding: 10,
-    opacity: 0.75,
+    backgroundColor: COLORS.NIGHTLIGHT_BLACK,
+    borderColor: COLORS.WHITE,
+    borderWidth: 1,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginTop: 10,
   },
-  name: {
-    fontFamily: Fonts.COMFORTAA_REGULAR,
-    fontSize: 24,
-    color: COLORS.WHITE,
-  },
-  phoneNumber: {
-    fontFamily: Fonts.COMFORTAA_REGULAR,
-    fontSize: 14,
-    color: COLORS.GRAY,
-    marginTop: 2,
-  },
-  profileStatsContainer: {
-    flexDirection: 'row',
-    marginTop: 20,
-  },
-  profileStatContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginVertical: 5,
-    // width: 75,  // Uncomment this to make the stats boxes the same width
-  },
-  profileStat: {
-    textAlign: 'center',
+  editProfileText: {
     fontFamily: Fonts.COMFORTAA_BOLD,
-    fontSize: 24,
-    color: COLORS.WHITE,
-  },
-  profileStatDesc: {
-    textAlign: 'center',
-    fontFamily: Fonts.COMFORTAA_REGULAR,
-    fontSize: 14,
-    color: COLORS.GRAY,
-  },
-  profileStatDivider: {
-    width: 1,
-    backgroundColor: COLORS.WHITE,
-    marginHorizontal: 20,
-  },
-  favoriteBarContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 30,
-  },
-  favoriteBarTextContainer: {
-    marginLeft: 10,
-  },
-  favoriteBarText: {
-    fontFamily: Fonts.COMFORTAA_BOLD,
-    fontSize: 20,
-    color: COLORS.WHITE,
-    marginVertical: 5,
-  },
-  favoriteBarDesc: {
-    fontFamily: Fonts.COMFORTAA_REGULAR,
     fontSize: 12,
+    color: COLORS.WHITE,
+  },
+  userInfoContainer: {
+    marginVertical: 10,
+  },
+  userName: {
+    fontFamily: Fonts.COMFORTAA_BOLD,
+    fontSize: 24,
+    color: COLORS.WHITE,
+    marginBottom: 5,
+  },
+  userDetailContainer: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+  },
+  userDetailIcon: {
+    width: 25,
+  },
+  userDetailText: {
+    fontFamily: Fonts.COMFORTAA_REGULAR,
+    fontSize: 14,
     color: COLORS.GRAY,
+    marginBottom: 5,
   },
-  emergencyContactsButton: {
-    // TODO: refactor this to work with home button
-    marginBottom:
-      SAFE_AREA_BOTTOM_MARGIN + NAVBAR_HEIGHT + EMERGENCY_BUTTON_RADIUS + 20,
-  },
+
+  // changeCoverButton: {
+  //   position: 'absolute',
+  //   bottom: 0,
+  //   padding: 10,
+  //   opacity: 0.75,
+  // },
+  // settingsButtonContainer: {
+  //   position: 'absolute',
+  //   right: 0,
+  //   opacity: 0.75,
+  // },
+  // settingsButton: {
+  //   padding: 10,
+  // },
+  // profileDetailsContainer: {
+  //   paddingTop: 150 / 2 + 10,
+  //   alignItems: 'center',
+  // },
+  // editProfileButton: {
+  //   position: 'absolute',
+  //   alignSelf: 'flex-end',
+  //   padding: 10,
+  //   opacity: 0.75,
+  // },
+  // name: {
+  //   fontFamily: Fonts.COMFORTAA_REGULAR,
+  //   fontSize: 24,
+  //   color: COLORS.WHITE,
+  // },
+  // phoneNumber: {
+  //   fontFamily: Fonts.COMFORTAA_REGULAR,
+  //   fontSize: 14,
+  //   color: COLORS.GRAY,
+  //   marginTop: 2,
+  // },
+  // profileStatsContainer: {
+  //   flexDirection: 'row',
+  //   marginTop: 20,
+  // },
+  // profileStatContainer: {
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   marginVertical: 5,
+  //   // width: 75,  // Uncomment this to make the stats boxes the same width
+  // },
+  // profileStat: {
+  //   textAlign: 'center',
+  //   fontFamily: Fonts.COMFORTAA_BOLD,
+  //   fontSize: 24,
+  //   color: COLORS.WHITE,
+  // },
+  // profileStatDesc: {
+  //   textAlign: 'center',
+  //   fontFamily: Fonts.COMFORTAA_REGULAR,
+  //   fontSize: 14,
+  //   color: COLORS.GRAY,
+  // },
+  // profileStatDivider: {
+  //   width: 1,
+  //   backgroundColor: COLORS.WHITE,
+  //   marginHorizontal: 20,
+  // },
+  // favoriteBarContainer: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   marginVertical: 30,
+  // },
+  // favoriteBarTextContainer: {
+  //   marginLeft: 10,
+  // },
+  // favoriteBarText: {
+  //   fontFamily: Fonts.COMFORTAA_BOLD,
+  //   fontSize: 20,
+  //   color: COLORS.WHITE,
+  //   marginVertical: 5,
+  // },
+  // favoriteBarDesc: {
+  //   fontFamily: Fonts.COMFORTAA_REGULAR,
+  //   fontSize: 12,
+  //   color: COLORS.GRAY,
+  // },
+  // emergencyContactsButton: {
+  //   // TODO: refactor this to work with home button
+  //   marginBottom:
+  //     SAFE_AREA_BOTTOM_MARGIN + NAVBAR_HEIGHT + EMERGENCY_BUTTON_RADIUS + 20,
+  // },
 });
--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -8,7 +8,11 @@ import {
   SafeAreaView,
   Alert,
 } from 'react-native';
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import {
+  MaterialCommunityIcons,
+  FontAwesome,
+  Ionicons,
+} from '@expo/vector-icons';
 import ProfileScreenStyles from '@nightlight/screens/profile/ProfileScreen.styles';
 import PartySvg from '@nightlight/components/svgs/PartySvg';
 import {
@@ -60,113 +64,190 @@ const ProfileScreen = ({ navigation }: BottomTabScreenProps) => {
   return (
     <ScrollView
       testID={TabRoute.PROFILE_STACK}
-      style={ProfileScreenStyles.scrollView}
-      contentContainerStyle={ProfileScreenStyles.scrollViewContainer}>
-      {/* Cover Picture */}
+      style={ProfileScreenStyles.scrollView}>
+      {/* Cover Pic */}
       <View style={ProfileScreenStyles.coverPicContainer}>
         <Image
           style={ProfileScreenStyles.coverPic}
           // TODO: make this dynamic
           source={require('@nightlight/assets/images/cover-photo.png')}
         />
-        {/* Change Cover Picture */}
-        <TouchableOpacity
-          onPress={handleChangeCoverPicture}
-          activeOpacity={0.5}
-          style={ProfileScreenStyles.changeCoverButton}>
-          <FontAwesome name='picture-o' size={25} color={COLORS.WHITE} />
-        </TouchableOpacity>
       </View>
 
-      {/* Settings Button */}
-      <SafeAreaView style={ProfileScreenStyles.settingsButtonContainer}>
-        <TouchableOpacity
-          onPress={handleSettingsPress}
-          activeOpacity={0.5}
-          style={ProfileScreenStyles.settingsButton}>
-          <Ionicons name='settings' size={25} color={COLORS.WHITE} />
-        </TouchableOpacity>
-      </SafeAreaView>
+      <View style={ProfileScreenStyles.contentContainer}>
+        <View style={ProfileScreenStyles.contentHeader}>
+          {/* Profile Pic */}
+          {user.imgUrlProfileSmall ? (
+            <Image
+              source={{ uri: user.imgUrlProfileSmall }}
+              style={ProfileScreenStyles.profilePic}
+            />
+          ) : (
+            <View style={ProfileScreenStyles.profilePic}>
+              <Text style={ProfileScreenStyles.initials}>{userInitials}</Text>
+            </View>
+          )}
 
-      {/* Profile Picture */}
-      {/* TODO: add press action?? (expand or edit?) */}
-      {user.imgUrlProfileSmall ? (
-        <Image
-          source={{ uri: user.imgUrlProfileSmall }}
-          style={ProfileScreenStyles.profilePic}
-        />
-      ) : (
-        <View style={ProfileScreenStyles.profilePic}>
-          <Text style={ProfileScreenStyles.initials}>{userInitials}</Text>
+          {/* Edit Button */}
+          <TouchableOpacity
+            onPress={handleEditProfile}
+            activeOpacity={0.75}
+            style={ProfileScreenStyles.editProfileButton}>
+            <Text style={ProfileScreenStyles.editProfileText}>
+              Edit Profile
+            </Text>
+          </TouchableOpacity>
         </View>
-      )}
-
-      {/* Profile Details */}
-      <View style={ProfileScreenStyles.profileDetailsContainer}>
-        {/* Edit Profile Button */}
-        <TouchableOpacity
-          onPress={handleEditProfile}
-          activeOpacity={0.5}
-          style={ProfileScreenStyles.editProfileButton}>
-          <FontAwesome name='pencil' size={25} color={COLORS.WHITE} />
-        </TouchableOpacity>
 
         {/* User Info */}
-        <Text style={ProfileScreenStyles.name}>
-          {user.firstName} {user.lastName}
-        </Text>
-        <Text style={ProfileScreenStyles.phoneNumber}>
-          {formatPhoneNumber(user.phone)}
-        </Text>
-
-        {/* Profile Statistics */}
-        <View style={ProfileScreenStyles.profileStatsContainer}>
-          <View style={ProfileScreenStyles.profileStatContainer}>
-            <Text style={ProfileScreenStyles.profileStat}>
-              {getNumFriends(userDocument)}
-            </Text>
-            <Text style={ProfileScreenStyles.profileStatDesc}>friends</Text>
+        <View style={ProfileScreenStyles.userInfoContainer}>
+          <Text style={ProfileScreenStyles.userName}>
+            {user.firstName} {user.lastName}
+          </Text>
+          <View style={ProfileScreenStyles.userDetailContainer}>
+            <MaterialCommunityIcons
+              name='email'
+              size={20}
+              color={COLORS.GRAY}
+              style={ProfileScreenStyles.userDetailIcon}
+            />
+            <Text style={ProfileScreenStyles.userDetailText}>{user.email}</Text>
           </View>
-          <View style={ProfileScreenStyles.profileStatDivider} />
-          <View style={ProfileScreenStyles.profileStatContainer}>
-            <Text style={ProfileScreenStyles.profileStat}>
-              {/* TODO: need field on user object */}
-              {2}
-            </Text>
-            <Text style={ProfileScreenStyles.profileStatDesc}>nights out</Text>
-          </View>
-          <View style={ProfileScreenStyles.profileStatDivider} />
-          <View style={ProfileScreenStyles.profileStatContainer}>
-            <Text style={ProfileScreenStyles.profileStat}>
-              {user.birthday.toLocaleDateString('en-US', { timeZone: 'UTC' })}
-            </Text>
-            <Text style={ProfileScreenStyles.profileStatDesc}>
-              your special day
+          <View style={ProfileScreenStyles.userDetailContainer}>
+            <FontAwesome
+              name='phone'
+              size={20}
+              color={COLORS.GRAY}
+              style={ProfileScreenStyles.userDetailIcon}
+            />
+            <Text style={ProfileScreenStyles.userDetailText}>
+              {formatPhoneNumber(user.phone)}
             </Text>
           </View>
-        </View>
-
-        {/* Favorite Bar */}
-        <View style={ProfileScreenStyles.favoriteBarContainer}>
-          <PartySvg />
-          <View style={ProfileScreenStyles.favoriteBarTextContainer}>
-            {/* TODO: need field on user doc */}
-            <Text style={ProfileScreenStyles.favoriteBarText}>Underground</Text>
-            <Text style={ProfileScreenStyles.favoriteBarDesc}>
-              seems to be your favorite bar these days
+          <View style={ProfileScreenStyles.userDetailContainer}>
+            <MaterialCommunityIcons
+              name='cake-variant'
+              size={20}
+              color={COLORS.GRAY}
+              style={ProfileScreenStyles.userDetailIcon}
+            />
+            <Text style={ProfileScreenStyles.userDetailText}>
+              {user.birthday.toLocaleString('en-US', {
+                timeZone: 'UTC',
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric',
+              })}
             </Text>
           </View>
         </View>
-
-        {/* Emergency Contacts Button */}
-        <Button
-          onPress={handleNavigateToEmergencyContacts}
-          text='See Emergency Contacts'
-          style={ProfileScreenStyles.emergencyContactsButton}
-        />
       </View>
     </ScrollView>
   );
 };
 
 export default ProfileScreen;
+
+// {/* Cover Picture */}
+// <View style={ProfileScreenStyles.coverPicContainer}>
+// <Image
+//   style={ProfileScreenStyles.coverPic}
+//   // TODO: make this dynamic
+//   source={require('@nightlight/assets/images/cover-photo.png')}
+// />
+//   {/* Change Cover Picture */}
+//   <TouchableOpacity
+//     onPress={handleChangeCoverPicture}
+//     activeOpacity={0.5}
+//     style={ProfileScreenStyles.changeCoverButton}>
+//     <FontAwesome name='picture-o' size={25} color={COLORS.WHITE} />
+//   </TouchableOpacity>
+// </View>
+
+// {/* Settings Button */}
+// <SafeAreaView style={ProfileScreenStyles.settingsButtonContainer}>
+//   <TouchableOpacity
+//     onPress={handleSettingsPress}
+//     activeOpacity={0.5}
+//     style={ProfileScreenStyles.settingsButton}>
+//     <Ionicons name='settings' size={25} color={COLORS.WHITE} />
+//   </TouchableOpacity>
+// </SafeAreaView>
+
+// {/* Profile Picture */}
+// {/* TODO: add press action?? (expand or edit?) */}
+// {user.imgUrlProfileSmall ? (
+//   <Image
+//     source={{ uri: user.imgUrlProfileSmall }}
+//     style={ProfileScreenStyles.profilePic}
+//   />
+// ) : (
+//   <View style={ProfileScreenStyles.profilePic}>
+//     <Text style={ProfileScreenStyles.initials}>{userInitials}</Text>
+//   </View>
+// )}
+
+// {/* Profile Details */}
+// <View style={ProfileScreenStyles.profileDetailsContainer}>
+//   {/* Edit Profile Button */}
+//   <TouchableOpacity
+//     onPress={handleEditProfile}
+//     activeOpacity={0.5}
+//     style={ProfileScreenStyles.editProfileButton}>
+//     <FontAwesome name='pencil' size={25} color={COLORS.WHITE} />
+//   </TouchableOpacity>
+
+//   {/* User Info */}
+//   <Text style={ProfileScreenStyles.name}>
+//     {user.firstName} {user.lastName}
+//   </Text>
+//   <Text style={ProfileScreenStyles.phoneNumber}>
+//     {formatPhoneNumber(user.phone)}
+//   </Text>
+
+//   {/* Profile Statistics */}
+//   <View style={ProfileScreenStyles.profileStatsContainer}>
+//     <View style={ProfileScreenStyles.profileStatContainer}>
+//       <Text style={ProfileScreenStyles.profileStat}>
+//         {getNumFriends(userDocument)}
+//       </Text>
+//       <Text style={ProfileScreenStyles.profileStatDesc}>friends</Text>
+//     </View>
+//     <View style={ProfileScreenStyles.profileStatDivider} />
+//     <View style={ProfileScreenStyles.profileStatContainer}>
+//       <Text style={ProfileScreenStyles.profileStat}>
+//         {/* TODO: need field on user object */}
+//         {2}
+//       </Text>
+//       <Text style={ProfileScreenStyles.profileStatDesc}>nights out</Text>
+//     </View>
+//     <View style={ProfileScreenStyles.profileStatDivider} />
+//     <View style={ProfileScreenStyles.profileStatContainer}>
+//       <Text style={ProfileScreenStyles.profileStat}>
+//         {user.birthday.toLocaleDateString('en-US', { timeZone: 'UTC' })}
+//       </Text>
+//       <Text style={ProfileScreenStyles.profileStatDesc}>
+//         your special day
+//       </Text>
+//     </View>
+//   </View>
+
+//   {/* Favorite Bar */}
+//   <View style={ProfileScreenStyles.favoriteBarContainer}>
+//     <PartySvg />
+//     <View style={ProfileScreenStyles.favoriteBarTextContainer}>
+//       {/* TODO: need field on user doc */}
+//       <Text style={ProfileScreenStyles.favoriteBarText}>Underground</Text>
+//       <Text style={ProfileScreenStyles.favoriteBarDesc}>
+//         seems to be your favorite bar these days
+//       </Text>
+//     </View>
+//   </View>
+
+//   {/* Emergency Contacts Button */}
+//   <Button
+//     onPress={handleNavigateToEmergencyContacts}
+//     text='See Emergency Contacts'
+//     style={ProfileScreenStyles.emergencyContactsButton}
+//   />
+// </View>
